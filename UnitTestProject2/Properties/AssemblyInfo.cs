e8d89f6d3e47d7c58b5aba3c740d3bd8755bb39a
kris675s <kris675s@eul.eal.dk>
2019-02-08 08:16:01 +0100

    Add project files.

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fec45f54-b79a-41f1-87b1-2260651b2dc6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateClassLibrary;
+
+namespace DateUnitTestProject
+{
+    [TestClass]
+    public class UnitTestDate
+    /*
+     * test types
+     * 
+     * arrange
+     * act
+     * assert
+     * 
+     * to types of deffenitions
+     * 
+     */
+    {
+        UclDate TestDate;
+
+        [TestInitialize]
+        public void TestInitalize()
+        {
+            TestDate = new UclDate(2019, 2, 6);
+        }
+        [TestMethod]
+        public void TestMethod_DateConstructor()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            Assert.IsNotNull(aDate, "UclDate Constructor is not working");
+        }
+        [TestMethod]
+        public void TestMethod_GetDay()
+        {
+            int day = TestDate.GetDay();
+            Assert.AreEqual(6, day);
+        }
+        [TestMethod]
+        public void TestMethod_GetWeek()
+        {
+            /*
+             * implementation error in getweek
+             * Message: Assert.AreEqual failed. Expected:<11>. Actual:<10>.
+             */
+            UclDate TestDate2 = new UclDate(2019, 2, 6);
+            ushort week = TestDate2.GetWeek();
+            Assert.AreEqual(6, week);
+        }
+        [TestMethod]
+        public void TestMethod_GetMonth()
+        {
+            int month = TestDate.GetMonth();
+            Assert.AreEqual(2, month);
+        }
+        [TestMethod]
+        public void TestMethod_GetYear()
+        {
+            int year = TestDate.GetYear();
+            Assert.AreEqual(2019, year);
+        }
+        [TestMethod]
+        public void TestMethod_GetDatoStringDMY()
+        {
+            string dmy = TestDate.GetDatoStringDMY();
+            Assert.AreEqual("6/2/2019", dmy);
+        }
+        [TestMethod]
+        /*
+         * syntax error
+         * Message: Assert.AreEqual failed. Expected:<2019/2/6>. Actual:<2019-2-6>.
+         */
+        public void TestMethod_GetDatoStringYMD()
+        {
+            string ymd = TestDate.GetDatoStringYMD();
+            Assert.AreEqual("2019/2/6", ymd);
+        }
+        [TestMethod]
+        /*
+         * syntax error 00-00-00 != 00/00/00
+         * Message: Assert.AreEqual failed. Expected:<2019/02/06>. Actual:<9-02-06>. 
+         */
+        public void TestMethod_GetDatoStringYMMDD()
+        {
+            string ymmdd = TestDate.GetDatoStringYMMDD();
+            Assert.AreEqual("2019/02/06", ymmdd);
+        }
+        [TestMethod]
+        public void TestMethod_GetDatoStringYMMDDAlternativ()
+        {
+            string ymmddalt = TestDate.GetDatoStringYMMDDAlternativ();
+            Assert.AreEqual("2019/06/02", ymmddalt);
+        }
+        [TestMethod]
+        public void TestMethod_GetDatoStringYMMDDAlternativ2()
+        {
+            string ymmddalt = TestDate.GetDatoStringYMMDDAlternativ();
+            Assert.AreEqual("2019-02-06", ymmddalt);
+        }
+    }
+}
+
